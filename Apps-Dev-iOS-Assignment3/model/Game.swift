56c19bd4d7e3a6ae2e86f337f5442a234f68b117
BILLFANLL <billfan0201@gmail.com>
2023-05-12 10:23:23 +1000

    creat model & update project file

---
+++ Apps-Dev-iOS-Assignment3/model/Game.swift
@@ -0,0 +1,12 @@
+//
+//  Game.swift
+//  Apps-Dev-iOS-Assignment3
+//
+//  Created by Bill Fan on 12/5/2023.
+//
+
+import Foundation
+import UIKit
+class Game {
+    
+}
---
+++ Apps-Dev-iOS-Assignment3/model/Shape.swift
@@ -0,0 +1,20 @@
+//
+//  Shape.swift
+//  Apps-Dev-iOS-Assignment3
+//
+//  Created by Bill Fan on 12/5/2023.
+//
+
+import Foundation
+import UIKit
+
+class Shape {
+    var shape = "triangle"
+    var color = UIColor.red
+    var level = 1
+    
+    func setColor (level:Int) {
+        
+    }
+
+}
