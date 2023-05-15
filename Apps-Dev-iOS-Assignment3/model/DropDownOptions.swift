d92bfd77ea2d4ca8620f969ce9d21bbf83a88b13
Joshua <Joshua.S.Dinkin@student.uts.edu.au>
2023-05-15 15:32:27 +1000

    DropDown Items

---
+++ Apps-Dev-iOS-Assignment3/model/DropDownOptions.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownOptions.swift
+//  Apps-Dev-iOS-Assignment3
+//
+//  Created by Joshua D on 15/5/2023.
+//
+
+import Foundation
+public class Options {
+    
+    let booleanData = ["true","false"]
+    let colours = ["blue","brown","green","orange","purple","red","white","yellow","black"]
+   let height = ["50","80","100","150","200"]
+   let ids = [50,80,100,150,200]
+    let shapes = ["triangle", "square", "rectangle", "circle"]
+}
+
